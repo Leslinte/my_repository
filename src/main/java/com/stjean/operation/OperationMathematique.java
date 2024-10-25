080ed161937b481fad5a33a4603465764e3cb21e
lesigne <leslinefoyet793@gmail.com>
2024-10-25 10:17:47 +0100

    factorial

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -4,5 +4,13 @@ public class OperationMathematique {
     public boolean estPositif(int nombre) {
         return nombre > 0;
     }
+    public int factorial(int number) {
+        if (number < 0) throw new IllegalArgumentException("Number must be non-negative");
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
 
 }
